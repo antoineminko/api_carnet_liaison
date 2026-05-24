9a47514ecd9563d9965e3e2c52b08631ba7d50dd
Antoine Minko <minkoantoinekelly@gmail.com>
2026-05-24 14:26:48 +0100

    fix migration order

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/migrate', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations exécutées avec succès !';
+});
