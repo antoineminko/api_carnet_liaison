80458c60d7b39925f0888002d8609b585f61191b
Antoine Minko <minkoantoinekelly@gmail.com>
2026-05-25 06:58:55 +0100

    Fix: save prof_principal_id on class creation

--- app/Http/Controllers/Api/ClasseController.php
+++ app/Http/Controllers/Api/ClasseController.php
@@ -34,13 +34,19 @@ class ClasseController extends Controller
                 'nom'      => $request->input('nom'),
                 'code'     => $request->input('code'),
                 'ecole_id' => $request->input('ecole_id'),
+                'prof_principal_id' => $request->input('prof_principal_id'),
                 'created_at' => now(),
                 'updated_at' => now(),
             ]);
 
             $classe = DB::table('classes')
                 ->leftJoin('ecoles', 'classes.ecole_id', '=', 'ecoles.id')
-                ->select('classes.*', 'ecoles.nom as ecole_nom')
+                ->leftJoin('enseignants', 'classes.prof_principal_id', '=', 'enseignants.id')
+                ->select(
+                    'classes.*', 
+                    'ecoles.nom as ecole_nom',
+                    DB::raw("CONCAT(enseignants.prenom, ' ', enseignants.nom) as prof_principal_nom")
+                )
                 ->where('classes.id', $id)
                 ->first();
 
